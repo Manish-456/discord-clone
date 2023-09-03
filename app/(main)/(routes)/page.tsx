c067206b36e944854af3234f5c73a582b74d5ebc
Manish <tamangmanish3456@gmail.com>
2023-09-03 14:43:46 +0545

    main group routing added

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24">
+  <h1>Hello sir</h1>
+  <Button>Hello Discord-Clone</Button>
+    </main>
+  );
+}
